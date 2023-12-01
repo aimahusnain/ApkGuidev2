59c1ca0da5ffeec62d9a88075f936b1288e92d46
aimahusnain <aimahusnain@gmail.com>
2023-12-01 16:22:48 +0500

    Import components of home using Dynamic Way

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,19 @@
 import {allBlogs} from "contentlayer/generated";
-import HomeCoverSection from "../components/Home/HomeCoverSection";
-import FeaturedPosts from "../components/Home/FeaturedPosts";
-import RecentPosts from "../components/Home/RecentPosts";
+// import HomeCoverSection from "../components/Home/HomeCoverSection";
+// import FeaturedPosts from "../components/Home/FeaturedPosts";
+// import RecentPosts from "../components/Home/RecentPosts";
+import dynamic from "next/dynamic"
+
+const RecentPosts = dynamic(()=>import('../components/Home/RecentPosts'))
+const FeaturedPosts = dynamic(()=>import('../components/Home/FeaturedPosts'))
+const HomeCoverSection = dynamic(()=>import('../components/Home/HomeCoverSection'))
 
 export default function Home() {
-  
   return (
     <main className="flex flex-col items-center justify-center">
       <HomeCoverSection blogs={allBlogs} />
       <FeaturedPosts blogs={allBlogs} />
       <RecentPosts blogs={allBlogs} />
-
 
     </main>
   )
